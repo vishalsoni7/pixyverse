de4320b278b460abb4e9064c6859b02d8f33900f
vishal soni <vishsoni043@gmail.com>
2023-06-11 18:23:23 +0530

    feature - added backend files.

---
+++ src/backend/db/users.js
@@ -0,0 +1,27 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Adarsh",
+    lastName: "Balika",
+    username: "adarshbalika",
+    password: "adarshBalika123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Vishal",
+    lastName: "Soni",
+    username: "vishalsoni",
+    password: "vishalsoni123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+];
