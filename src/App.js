1e9542314c327b1544d36f7739e734bee6cbb870
vishal soni <vishsoni043@gmail.com>
2023-06-23 18:51:06 +0530

    docs

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import "./App.css";
 import { AppRouts } from "./routes/Route";
 
 import { Toaster } from "react-hot-toast";
-import { Feed } from "./pages/feed/Feed";
-import { Users } from "./pages/users/Users";
 
 function App() {
   return (
     <div className="App">
       <Toaster position="bottom-right" reverseOrder={false} />
-      {/* <Feed /> */}
+
       <AppRouts />
-      {/* <Users /> */}
     </div>
   );
 }
